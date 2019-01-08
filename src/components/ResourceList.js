1430615c4ee61999436002196fec7ff574b35980
caprica-Six <mogwais@outlook.com>
2019-01-08 15:17:53 +0000

    refactor and add useEffect to get access to life-cycle methods

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,15 +1,23 @@
 import React, {useState, useEffect } from 'react'
 import axios from "axios";
 
-const ResourceList = () => {
-    const [resources, setResources] = useState([]);
+const ResourceList = ({ resource }) => {
+    // have access to the initial state resources on const resources
+    // can be changed using setResources()
+    // initial value of empty array
+    const [resources, setResources] = useState([]); 
 
-    const fetchResource = async () => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+    const fetchResource = async (resource) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
-        this.setState({ resources: response.data}); // data is an axios object
+        setResources(response.data);
     }
 
+    // get access to life-cycle methods with useEffect()
+    useEffect(() => {
+        fetchResource(resource);        
+    }, [])
+    
     return (
         // Test number of resources fetched
         <div>{resources.length}</div>
