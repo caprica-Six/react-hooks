ca1b91e84a64f0ccbcc55eeaaad0993964f222be
caprica-Six <mogwais@outlook.com>
2019-01-08 15:03:18 +0000

    refactor a Class to Function & use useEffect to remove duplication & update life-cycle hook

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,38 +1,19 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect } from 'react'
 import axios from "axios";
 
-class ResourceList extends Component {
-    // se state to fetch all the resources from the API
-    state = { resources: [] };
+const ResourceList = () => {
+    const [resources, setResources] = useState([]);
 
-    async componentDidMount() {
+    const fetchResource = async () => {
         const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
 
         this.setState({ resources: response.data}); // data is an axios object
     }
 
-    // to render todos, we have to add anothe rlife-cycle hook
-    // componentDidMount() gets called anytime the component re-renders because of a current component or setState() inside this class-based comp.
-    // anytime the componentDidUpdate() updates, will be called with prevProps
-    async componentDidUpdate(prevProps) {
-        // console.log(prevProps);
-        //check if the resource type has changed, then we have to make another request!
-        
-        if(prevProps.resource !== this.props.resource) {
-            const response = await axios.get(
-                `https://jsonplaceholder.typicode.com/${this.props.resource}`
-            );
-
-            this.setState({ resources: response.data });
-        }
-    }
-
-    render() {
-        return (
-            // Test number of resources fetched
-            <div>{this.state.resources.length}</div>
-        )
-    }
+    return (
+        // Test number of resources fetched
+        <div>{resources.length}</div>
+    )
 }
 
 export default ResourceList;
